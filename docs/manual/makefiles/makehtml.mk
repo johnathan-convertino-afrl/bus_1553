611896f63c137e15e212c380000fdbcc18b384eb
Jay Convertino <johnathan.convertino.1@us.af.mil>
2024-11-13 10:19:15 -0500

    regen and remove wiki feature

--- docs/manual/makefiles/makehtml.mk
+++ docs/manual/makefiles/makehtml.mk
@@ -11,6 +11,7 @@ HTML_DIR=html
 ND_HTML=$(HTML_DIR)/index.html
 ND_HTML_FILES=$(wildcard $(HTML_DIR)/files/*.html)
 
+LAT_COM_SRC=$(SRC_DIR)/common.tex
 LAT_HTML_SRC=$(SRC_DIR)/html.tex
 LAT_HTML_HTML=$(LAT_HTML_SRC:.tex=.html)
 LAT_HTML_CSS=$(LAT_HTML_SRC:.tex=.css)
